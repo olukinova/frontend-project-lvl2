f263baaebe267184da420ceb1496ae9d7a956254
Olena Lukinova <95899106+olukinova@users.noreply.github.com>
2022-06-10 17:31:55 -0700

    add 'option' to the gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,4 +5,5 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-v, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
 program.parse(process.argv);
